c8b0d1140814d5c344bbc119ec3a14c0e270bd98
C:\Users\as.EMSDOMAIN\AppData\Roaming\The Bat! <C:\Users\as.EMSDOMAIN\AppData\Roaming\The Bat!@localhost>
2011-05-24 18:14:45 +0600

    start activity intent to items list

--- src/com/pyjioh/activity/MultiShinaStart.java
+++ src/com/pyjioh/activity/MultiShinaStart.java
@@ -3,7 +3,9 @@ package com.pyjioh.activity;
 import com.pyjioh.R;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MultiShinaStart extends Activity {
 	
@@ -13,6 +15,10 @@ public class MultiShinaStart extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 		
+	}
+	
+	public void onClick(View v) {
+		startActivity(new Intent(this, MultiShinaItemList.class));
 	}
 
 }
